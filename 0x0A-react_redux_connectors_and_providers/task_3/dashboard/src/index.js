92c4574b3896ea873a10470310c921bcf935a7aa
Tsion Moges <tsionmoges25@gmail.com>
2023-03-19 12:58:02 +0300

    Update index.js

--- 0x0A-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
+++ 0x0A-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
@@ -1,23 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { creatStore, Provider, applyMiddleware, compose} from "react-redux";
-import thunk from 'redux-thunk'
-import App from "./App/App";
-import { uiReducer } from "./reducers/uiReducer";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import App from './App/App';
+import uiReducer, { initialState } from './reducers/uiReducer';
+import { Map } from 'immutable';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = creatStore(uiReducer, composeEnhancers(applyMiddleware(thunk)))
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+const store = createStore(
+	uiReducer,
+	Map(initialState),
+	composeEnhancers(applyMiddleware(thunk))
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+ReactDOM.render(
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
+);
